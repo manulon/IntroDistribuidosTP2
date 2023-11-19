2e7240152a2f20db93d01a0342a1f94f466c73d4
adrianromero99 <adromero@fi.uba.ar>
2023-11-19 19:54:51 -0300

    feat: Cambios en topolgia

---
+++ Topology.py
@@ -0,0 +1,60 @@
+from mininet.topo import Topo
+
+class Topology(Topo):
+    
+    def __init__(self, switches=1):
+        Topo.__init__(self)
+
+        switches = self.validSwitches(switches)
+        print("Cantidad de switches: %s" % (switches))
+     
+        print("Agregamos los hosts h1, h2, h3 y h4")
+        hostIzquierda1 = self.addHost('h1')
+        hostIzquierda2 = self.addHost('h2')
+        hostDerecha3 = self.addHost('h3')
+        hostDerecha4 = self.addHost('h4')
+
+        print("Agregamos el switch: s0")
+        primerSwitch = self.addSwitch('s0')
+        ultimoSwitch = primerSwitch
+
+        for nroSwitch in range(1, switches):
+            nombreSwitch = 's%i' % (nroSwitch)
+            print("Agregamos el switch: %s" % (nombreSwitch))
+            switchActual = self.addSwitch(nombreSwitch)
+
+            print("Agreamos un link entre los swtiches: %s y %s" % (ultimoSwitch, switchActual))
+            self.addLink(ultimoSwitch, switchActual)
+            
+            ultimoSwitch = switchActual
+
+        print("Agregamos un link entre el host: %s y el switch %s (primer switch)" % (hostIzquierda1, primerSwitch))
+        self.addLink(hostIzquierda1, primerSwitch)
+
+        print("Agregamos un link entre el host: %s y el switch %s (primer switch)" % (hostIzquierda2, primerSwitch))
+        self.addLink(hostIzquierda2, primerSwitch)
+
+        print("Agregamos un link entre el host: %s y el switch %s (ultimo switch)" % (hostDerecha3, ultimoSwitch))
+        self.addLink(hostDerecha3, ultimoSwitch)
+
+        print("Agregamos un link entre el host: %s y el switch %s (ultimo switch)" % (hostDerecha4, ultimoSwitch))
+        self.addLink(hostDerecha4, ultimoSwitch)
+
+
+    def validSwitches(self, switches):
+        isSwitchesOK = False
+        validSwitches = None
+
+        while not isSwitchesOK:
+            try:
+                validSwitches = int(switches)
+                if validSwitches < 1:
+                    raise Exception
+                isSwitchesOK = True
+            except:
+                print("Ocurrió un error con la cantidad de switches. La cantidad de switched debe ser un entero mayor o igual que 1")
+                switches = input("Indique la cantidad de switches: ") 
+              
+        return validSwitches
+
+topos = {'topology': (lambda switches:  Topology(switches))}
